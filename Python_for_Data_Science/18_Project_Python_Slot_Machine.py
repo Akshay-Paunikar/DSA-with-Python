4da0a4d8d5be3276a91ba9ce1fc63d717504464b
Akshay Paunikar <akshayakki29@gmail.com>
2024-03-27 00:37:35 +0530

    slot machine symbols working tested successfully

--- Python_for_Data_Science/18_Project_Python_Slot_Machine.py
+++ Python_for_Data_Science/18_Project_Python_Slot_Machine.py
@@ -33,6 +33,15 @@ def get_slot_machine_spin(rows, cols, symbols):
         
     return columns
 
+def print_slot_machine(columns):
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns) - 1:
+                print(column[row], end=" | ")
+            else:
+                print(column[row], end="")
+
+        print()
 
 # below function is useful for collecting user input that gets the deposit from the user.
 def deposit():
@@ -94,5 +103,8 @@ def main():
         else:
             break            
     print(f"You are betting ${bet} on {lines} lines. Total bet is equal to ${total_bet}")
+    
+    slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
+    print_slot_machine(slots)
     
 main()
