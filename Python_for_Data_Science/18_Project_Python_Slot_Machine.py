85bac8ad6db39f0b73a62f2055c78f29950c223b
Akshay Paunikar <akshayakki29@gmail.com>
2024-03-26 23:49:35 +0530

    Number of lines to bet on tested successfully

--- Python_for_Data_Science/18_Project_Python_Slot_Machine.py
+++ Python_for_Data_Science/18_Project_Python_Slot_Machine.py
@@ -1,4 +1,6 @@
-
+MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
 
 # below function is useful for collecting user input that gets the deposit from the user.
 def deposit():
@@ -15,5 +17,29 @@ def deposit():
             
     return amount
 
+# below function will ask players how much amount to bet and on how much on each lines they want to bet on.
+def get_number_of_lines():
+    while True:
+        lines = input(
+            "Enter the number of lines to bet on (1 - "+ str(MAX_LINES) + ")? ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print("Enter a valid number of lines.")
+        else:
+            print("Please enter a number.")
+            
+    return lines
+
+
+
 # let's check if our function works as it is intended or not
-deposit()
+# deposit()
+
+def main():
+    balance = deposit()
+    lines = get_number_of_lines()
+    print(balance, lines)
+main()
