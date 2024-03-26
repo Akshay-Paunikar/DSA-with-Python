755b43762dc4a89c12bf91688830f596230ad7c5
Akshay Paunikar <akshayakki29@gmail.com>
2024-03-27 01:08:11 +0530

    Python Slot Machine Project Done

--- Python_for_Data_Science/18_Project_Python_Slot_Machine.py
+++ Python_for_Data_Science/18_Project_Python_Slot_Machine.py
@@ -110,26 +110,37 @@ def get_bet():
             
     return amount
     
-
-# let's check if our function works as it is intended or not
-# deposit()
-
-def main():
-    balance = deposit()
+def spin(balance):
     lines = get_number_of_lines()
     while True:
         bet = get_bet()
         total_bet = bet * lines
+
         if total_bet > balance:
-            print(f"You do not have enough balance to bet that amount, your current balance is ${balance}.")
+            print(
+                f"You do not have enough to bet that amount, your current balance is: ${balance}")
         else:
-            break            
-    print(f"You are betting ${bet} on {lines} lines. Total bet is equal to ${total_bet}")
-    
+            break
+
+    print(
+        f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet}")
+
     slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
     print_slot_machine(slots)
     winnings, winning_lines = check_winnings(slots, lines, bet, symbol_value)
     print(f"You won ${winnings}.")
     print(f"You won on lines:", *winning_lines)
-    
+    return winnings - total_bet
+
+def main():
+    balance = deposit()
+    while True:
+        print(f"Current balance is ${balance}")
+        answer = input("Press enter to play (q to quit).")
+        if answer == "q":
+            break
+        balance += spin(balance)
+
+    print(f"You left with ${balance}")
+
 main()
