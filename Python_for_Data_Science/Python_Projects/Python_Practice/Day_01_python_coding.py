e94c7cc565b85d33cd5bf86b57b0e9c03598b677
Akshay Paunikar <akshayakki29@gmail.com>
2025-08-13 15:25:07 +0530

    count vowels in the string & ignore the case

--- Python_for_Data_Science/Python_Projects/Python_Practice/Day_01_python_coding.py
+++ Python_for_Data_Science/Python_Projects/Python_Practice/Day_01_python_coding.py
@@ -12,3 +12,20 @@ print(reverse_string("Hello, World!"))  # Output: !dlroW ,olleH
 print(reverse_string("Python"))       # Output: "nohtyP"
 print(reverse_string("Level"))        # Output: "leveL"
 print(reverse_string("akshay"))        # Output: "yahska"
+print("********** problem 1 completed **********")
+
+# Problem 2: Count Vowels in a String: Create a function that counts the number of vowels (a, e, i, o, u) in a given string. Ignore case.
+def count_vowels(text):
+    vowels = "aeiouAEIOU"
+    count = 0
+    for i in text:
+        if i in vowels:
+            count += 1
+    return count
+
+# Example usage
+print(count_vowels("Hello, World!"))  # Output: 3
+print(count_vowels("Python"))         # Output: 1
+print(count_vowels("AEIOUaeiou"))     # Output: 10
+print(count_vowels("xyz"))            # Output: 0
+print("********** problem 2 completed **********")
