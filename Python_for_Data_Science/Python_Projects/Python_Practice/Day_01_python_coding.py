85211f44516d6a56bb25d35e87ac2c34396a7deb
Akshay Paunikar <akshayakki29@gmail.com>
2025-08-13 15:51:56 +0530

    remove duplicates from a list without using set() while preserving the original order

--- Python_for_Data_Science/Python_Projects/Python_Practice/Day_01_python_coding.py
+++ Python_for_Data_Science/Python_Projects/Python_Practice/Day_01_python_coding.py
@@ -13,6 +13,7 @@ print(reverse_string("Python"))       # Output: "nohtyP"
 print(reverse_string("Level"))        # Output: "leveL"
 print(reverse_string("akshay"))        # Output: "yahska"
 print("********** problem 1 completed **********")
+print("")
 
 # Problem 2: Count Vowels in a String: Create a function that counts the number of vowels (a, e, i, o, u) in a given string. Ignore case.
 def count_vowels(text):
@@ -29,3 +30,20 @@ print(count_vowels("Python"))         # Output: 1
 print(count_vowels("AEIOUaeiou"))     # Output: 10
 print(count_vowels("xyz"))            # Output: 0
 print("********** problem 2 completed **********")
+print("")
+
+# Problem 3: Remove Duplicates from a List:
+# Write a function that removes duplicates from a list without using set() while preserving the original order.
+def remove_duplicates(lst):
+    unique_list = []
+    for item in lst:
+        if item not in unique_list:
+            unique_list.append(item)
+    return unique_list
+
+# Example usage
+print(remove_duplicates([1, 2, 3, 2, 1, 4, 5]))  # Output: [1, 2, 3, 4, 5]
+print(remove_duplicates(["apple", "banana", "apple", "orange"]))  # Output: ["apple", "banana", "orange"]
+print(remove_duplicates(["a", "b", "a", "c"])) # Output: ["a", "b", "c"]
+print("********** problem 3 completed **********")
+print("")
